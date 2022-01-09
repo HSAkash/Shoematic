f7914330b0caeea1124f33c6c2fa06b8fcdff0a2
HSAkash <hemelakash472@gmail.com>
2022-01-09 14:59:53 +0600

    service mail

--- Shoematic/settings.py
+++ Shoematic/settings.py
@@ -200,5 +200,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dj@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'shoematic.service@gmail.com'
+EMAIL_HOST_PASSWORD = 'Shoematic.Service07'
