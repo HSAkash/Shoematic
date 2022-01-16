894f8d7402cc38cac56502c60c41dddaf28aa745
HSAkash <hemelakash472@gamil.com>
2022-01-16 12:10:22 +0600

    fix some image issue

--- Shoematic/settings.py
+++ Shoematic/settings.py
@@ -173,17 +173,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/images/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 
-# MEDIA_ROOT = BASE_DIR / 'static/images'
-MEDIA_ROOT = BASE_DIR / 'static/images'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images'),
 
 
 # Default primary key field type
